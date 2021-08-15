bcd038f20e34e4cb263a6300ea926c30d005da53
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-08-14 23:32:18 -0400

    preliminary structure for controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require('../models');
+
+// get all thoughts
+
+// get a signle thought by id
+
+// post to create a new thought, with new thought added to user's thought array field
+
+// put to update a thought by id
+
+// delete a thought by id
+
+// REACTION-RELATED ROUTES //
+
+// post to create a reaction store in a single thought's reactions array field
+
+// delete to remove a reaction by reactionID
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,21 @@
+const { User } = require('../models');
+
+// get all users
+
+// get user by id
+
+// post new user
+
+// put to updare user by id
+
+// delete to remove user by id
+
+// bonus: remove associated thoughts when deleted
+
+// FRIEND-RELATED ROUTES //
+
+// post to add a new friend to a user's friend list
+
+// delete to remove a friend from a user's friend list
+
+module.exports = userController;

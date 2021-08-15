aa22aea053c29e279424b556a2974182fd7202f0
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-08-15 12:45:21 -0400

    routes started for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,21 +1,66 @@
 const { User } = require('../models');
 
-// get all users
+const userController = {
+    // get all users
+    getAllUser (req, res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // get user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found by this id.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // post new user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+    // put to update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found by this id.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    // delete to remove user by id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found by this id.' })
+                }
+                res.status(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+    // bonus: remove associated thoughts when deleted
 
-// get user by id
+    // FRIEND-RELATED ROUTES //
 
-// post new user
+    // post to add a new friend to a user's friend list
 
-// put to updare user by id
-
-// delete to remove user by id
-
-// bonus: remove associated thoughts when deleted
-
-// FRIEND-RELATED ROUTES //
-
-// post to add a new friend to a user's friend list
-
-// delete to remove a friend from a user's friend list
+    // delete to remove a friend from a user's friend list
+};
 
 module.exports = userController;

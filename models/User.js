3316079f813a8d079515afb857fc8f570d3335b9
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-08-14 22:28:35 -0400

    more file structure and updates to user model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+
+const UserSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            //matching email
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    // function to retrieve length of the user's friends array field on query
+});
+
+const User = model('User', UserSchema);
+
+module.exports = Pizza;
